566364d41c2916dfb09a2df0cad846752742c833
minahow3 <minahow3@gmail.com>
2024-01-04 17:38:33 +0900

    Added alarm function to AlarmScreen.js

--- screens/AlarmScreen.js
+++ screens/AlarmScreen.js
@@ -1,66 +1,122 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Button } from 'react-native';
-import DateTimePicker from '@react-native-community/datetimepicker';
+import { View, Text, TouchableOpacity,Switch, StyleSheet, Image,Button} from 'react-native';
 import { Audio } from 'expo-av';
-import alarmSound from '../voice/voice1.mp3'; // ファイルのパスを正しく指定
+import DateTimePicker from '@react-native-community/datetimepicker';
+import Modal from 'react-native-modal';
 
 const AlarmScreen = () => {
-  const [selectedTime, setSelectedTime] = useState(new Date());
+  const [currentTime, setCurrentTime] = useState(new Date());
+  const [alarms, setAlarms] = useState([
+    { time: new Date(), isActive: false },
+    { time: new Date(), isActive: false },
+    { time: new Date(), isActive: false },
+  ]);
   const [showPicker, setShowPicker] = useState(false);
-
-  const sound = new Audio.Sound();
-  console.log(alarmSound)
-
+  const [sound, setSound] = useState();
+  const [alarmRinging, setAlarmRinging] = useState(false);
+  const [selectedAlarmIndex, setSelectedAlarmIndex] = useState(0);
+  const [editedAlarmIndex, setEditedAlarmIndex] = useState(null);
+  const [showPopup, setShowPopup] = useState(false);
+  const [alarmIntervalId, setAlarmIntervalId] = useState(null)
 
   useEffect(() => {
-    return () => {
-      // コンポーネントがアンマウントされる時にサウンドをアンロード
-      sound.unloadAsync();
-    };
-  }, []);
+    // 現在時刻の更新とアラームのチェックを1秒ごとに行う
+    const intervalId = setInterval(() => {
+      setCurrentTime(new Date());
+      checkAlarms();
+    }, 1000);
 
-  const showDateTimePicker = () => {
+    // コンポーネントのアンマウント時にクリーンアップ
+    return () => {
+      clearInterval(intervalId);
+      if (sound) {
+        sound.unloadAsync();
+      }
+    };
+  }, [alarms, sound]);
+
+  const showDateTimePicker = (index) => {
     setShowPicker(true);
+    setEditedAlarmIndex(index);
   };
 
   const hideDateTimePicker = () => {
     setShowPicker(false);
   };
 
+  // アラーム更新
   const handleDateChange = (event, date) => {
-    setSelectedTime(date);
+    const updatedAlarms = [...alarms];
+    updatedAlarms[editedAlarmIndex].time = date;
+    setAlarms(updatedAlarms);
   };
-
-  const handleSave = async () => {
+  const handleSave = () => {
     hideDateTimePicker();
-    console.log('保存されました:', selectedTime);
-
-    // アラームを設定する場合、ここで設定された時間に音声を再生する処理を呼び出す
-    await setAlarm(selectedTime);
+    // ここでアラームの時間を保存するなどの処理を行う
+    console.log('保存されました:', alarms[editedAlarmIndex].time);
   };
 
-  const setAlarm = async (alarmTime) => {
-    const currentDate = new Date();
-    const timeDifference = alarmTime.getTime() - currentDate.getTime();
+  const toggleAlarm = (index) => {
+    const updatedAlarms = [...alarms];
+    updatedAlarms[index].isActive = !updatedAlarms[index].isActive;
+    setAlarms(updatedAlarms);
+  };
 
-    if (timeDifference > 0) {
-      // アラームの設定時間が未来の場合
-      setTimeout(async () => {
-        // 指定の音声を再生
-        await playAlarmSound();
-      }, timeDifference);
+  const checkAlarms = async () => {
+    for (const alarm of alarms) {
+      const alarmTime = alarm.time;
+      const currentTime = new Date();
+
+      // アラームの設定された時間（時、分）が現在の時間と一致するかどうかを確認
+      if (
+        alarm.isActive &&
+        alarmTime.getHours() === currentTime.getHours() &&
+        alarmTime.getMinutes() === currentTime.getMinutes()
+      ) {
+        console.log('checkAlarms');
+        alarm.isActive = !alarm.isActive;
+        setShowPopup(true); // アラームが鳴っている場合、ポップアップを表示
+        // 10秒ごとに音声を再生
+        const intervalId = setInterval(() => {
+          playAlarmSound();
+        }, 10000);
+
+        // 1分後に音声再生を停止
+        setTimeout(() => {
+          clearInterval(intervalId);
+          setShowPopup(false);
+          stopAlarm();
+        }, 60000);
+
+        // stopAlarm() を呼ぶために intervalId を state に保存
+        setAlarmIntervalId(intervalId);
+      }
     }
   };
 
   const playAlarmSound = async () => {
     try {
-      // サウンドを読み込む
-      await sound.loadAsync(alarmSound);
-
-      // サウンドを再生
-      await sound.playAsync();
+      const { sound } = await Audio.Sound.createAsync(
+        require('../voice/voice1.mp3'),
+        { shouldPlay: true }
+      );
+      setSound(sound);
     } catch (error) {
       console.error('音声ファイルの読み込みまたは再生に失敗しました', error);
+    }
+  };
+
+  const stopAlarm = async () => {
+    if (sound) {
+      clearInterval(alarmIntervalId); // インターバルの停止
+      await sound.unloadAsync();
+    }
+  };
+
+  const handlePopupButtonPress = (stopAlarm) => {
+    setShowPopup(false);
+    if (stopAlarm) {
+      stopAlarm();
     }
   };
 
@@ -68,29 +124,57 @@ const AlarmScreen = () => {
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.headerText}>アラーム</Text>
+        <Text style={styles.currentTimeText}>
+          現在の時刻: {currentTime.toLocaleTimeString('ja-JP', { hour: '2-digit', minute: '2-digit', second: '2-digit' })}
+        </Text>
       </View>
-      <View style={styles.timeContainer}>
-        <TouchableOpacity onPress={showDateTimePicker}>
-          <Text style={styles.timeText}>
-            {selectedTime.toLocaleTimeString('ja-JP', { hour: '2-digit', minute: '2-digit' })}
-          </Text>
-        </TouchableOpacity>
-        {showPicker && (
-          <>
-            <DateTimePicker
-              value={selectedTime}
-              mode="time"
-              is24Hour={true}
-              display="spinner"
-              onChange={handleDateChange}
+      <View style={styles.alarmsContainer}>
+        {alarms.map((alarm, index) => (
+          <View key={index} style={styles.alarmItem}>
+            <Text style={styles.alarmTimeText}>
+              アラーム {index + 1} :  {alarm.time.toLocaleTimeString('ja-JP', { hour: '2-digit', minute: '2-digit' })}
+            </Text>
+            <TouchableOpacity onPress={() => showDateTimePicker(index)}>
+              <Image source={require('../assets/edit.png')} style={styles.editIcon} />
+            </TouchableOpacity>
+            <Switch
+              value={alarm.isActive}
+              onValueChange={() => toggleAlarm(index)}
+              thumbColor={alarm.isActive ? 'white' : 'white'}
+              trackColor={{ false: '#8E8E93', true: '#007AFF' }}
+              ios_backgroundColor="#8E8E93"
             />
-            <Button title="保存" onPress={handleSave} />
-          </>
-        )}
+          </View>
+        ))}
       </View>
+      {showPicker && (
+        <>
+        <DateTimePicker
+          value={alarms[selectedAlarmIndex].time}
+          mode="time"
+          is24Hour={true}
+          display="spinner"
+          onChange={handleDateChange}
+          />
+          <Button title="保存" onPress={handleSave} />
+        </>
+      )}
+      {showPopup && (
+        <Modal isVisible={showPopup} style={styles.modal}>
+          <View style={styles.popupContainer}>
+            <Text style={styles.popupText}>アラームを停止しますか？</Text>
+            <TouchableOpacity
+              style={[styles.popupButton, { backgroundColor: '#007AFF' }]}
+              onPress={() => handlePopupButtonPress(stopAlarm)}
+            >
+              <Text style={styles.popupButtonText}>はい</Text>
+            </TouchableOpacity>
+          </View>
+        </Modal>
+      )}
     </View>
   );
-};
+      }
 
 const styles = StyleSheet.create({
   container: {
@@ -105,14 +189,68 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
   },
-  timeContainer: {
+  currentTimeText: {
+    fontSize: 18,
+    marginTop: 10,
+  },
+  alarmsContainer: {
+    marginTop: 20,
+  },
+  alarmItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    margin:15
+  },
+  alarmTimeText: {
+    fontSize: 18,
+  },
+  editIcon: {
+    width: 20,
+    height: 20,
+    marginLeft: 10,
+  },
+  alarmToggleIcon: {
+    width: 30,
+    height: 30,
+    marginLeft: 10,
+  },
+  stopAlarmContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
-  timeText: {
-    fontSize: 48,
-    fontWeight: 'bold',
+  stopAlarmText: {
+    fontSize: 18,
+    marginBottom: 20,
+  },
+  stopAlarmButton: {
+    fontSize: 18,
+    color: 'blue',
+  },
+  // ポップアップ用のスタイル
+  modal: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  popupContainer: {
+    backgroundColor: 'black',
+    padding: 20,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  popupText: {
+    fontSize: 18,
+    marginBottom: 20,
+    color: 'white',
+  },
+  popupButton: {
+    padding: 10,
+    borderRadius: 5,
+  },
+  popupButtonText: {
+    color: 'white',
+    fontSize: 16,
   },
 });
 
